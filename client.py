1b2a50be2a90ad72811316e1b3b825dceaa690dc
CarsonWoodford <carsonwoodford@gmail.com>
2018-10-26 16:19:29 -0600

    Files added
    The original files as proof of concept. Most likely will be replaced with files that utilize ftp.

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print (sys.stderr, 'connecting to %s port %s' % server_address)
+sock.connect(server_address)
+
+try:
+    
+    # Send data
+    img = open("IMG_2957.JPG", "rb")
+    sock.sendall(img.read())
+
+finally:
+    print (sys.stderr, 'closing socket')
+    img.close()
+    sock.close()
