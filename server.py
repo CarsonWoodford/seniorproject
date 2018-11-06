eeaed40d315dc16ac8bb41295279b2d123dac744
CarsonWoodford <carsonwoodford@gmail.com>
2018-11-05 18:13:46 -0700

    Updated Server Code
    Added functionality to display a webpage, and cleaned up some code.

--- server.py
+++ server.py
@@ -1,38 +1,38 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
 server_address = ('localhost', 10000)
 print (sys.stderr, 'starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
-# Listen for incoming connections
 sock.listen(1)
 
-temp = bytearray()
+lastCount = 0
 while True:
-    # Wait for a connection
-
     print (sys.stderr, 'waiting for a connection')
     connection, client_address = sock.accept()
+    
     try:
-        print (sys.stderr, 'connection from', client_address)
-        temp = bytearray()
-        while True:
-            data = connection.recv(3000)
-            temp += data
-            if not data:
-                print (sys.stderr, 'no more data from', client_address)
-                break           
-    finally:
-        # Clean up the connection
-        image = open("mytemp.jpg", "wb")
-        image.write(temp)
-        image.close()
-        connection.close()
-        temp = bytearray()
-        break
+        clientType = connection.recv(1000).decode()
+        if clientType == "SERVICER":
+            temp = bytearray()
+            while True:
+                data = connection.recv(3000)
+                temp += data
+                if not data:
+                    print (sys.stderr, 'no more data from', client_address)
+                    break
+            image = open("mytemp.jpg", "wb")
+            image.write(temp)
+            image.close()
+        else:
+            http_response = """HTTP/1.1 200 OK
 
+"""
+            #append html here
+            http_response += "<body>There were no cars seen</body>"
+            connection.sendall(str.encode(http_response))
+    finally:
+        connection.close()
